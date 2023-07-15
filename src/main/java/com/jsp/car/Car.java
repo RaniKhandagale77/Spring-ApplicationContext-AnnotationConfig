8572b379dfb84a30bb2cc47e4c7ce7100fd8a9c8
ranikhandagale77 <ranikhandagale77@gmail.com>
2023-07-15 15:13:54 +0530

    Spring-ApplicationContext-AnnotationConfig

---
+++ src/main/java/com/jsp/car/Car.java
@@ -0,0 +1,11 @@
+package com.jsp.car;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+	public Car() {
+		System.out.println("Car Object is created........");
+	}
+}
---
+++ src/main/java/com/jsp/car/MyConfig.java
@@ -0,0 +1,11 @@
+package com.jsp.car;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.jsp.car")
+public class MyConfig {
+
+	
+}
